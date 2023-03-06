2687e9f9c5079c80412b237e8d79b85bf5baf229
Aki Wada <115308304+akiwadajp@users.noreply.github.com>
2023-03-05 22:48:27 -0600

    Add files via upload

---
+++ caesar1.py
@@ -0,0 +1,16 @@
+alpha =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+str_in = input("Enter message, like HELLO: ")
+
+n = len(str_in)
+str_out = ""
+
+for i in range(n):
+   c = str_in[i]
+   loc = alpha.find(c)
+   print(i, c, loc) 
+   newloc = loc + 14
+   str_out += alpha[newloc]
+   print(newloc, str_out)
+
+print("Obfuscated version:", str_out)
